f6ac684cce3e145d2a96a83889bf400ff92a1384
iimuz <iz.primary.story+github@gmail.com>
2019-02-22 09:41:58 +0900

    fix: model のダウンロードスクリプトを修正

--- word/download_model.sh
+++ word/download_model.sh
@@ -2,8 +2,10 @@
 #
 # モデルデータをダウンロードします。
 
-wget http://public.shiroyagi.s3.amazonaws.com/latest-ja-word2vec-gensim-model.zip -o model/model.zip
-pushd model
+DST_DIR=word_to_similarity/model
+mkdir -p $DST_DIR
+wget http://public.shiroyagi.s3.amazonaws.com/latest-ja-word2vec-gensim-model.zip -O $DST_DIR/model.zip
+pushd $DST_DIR
 unzip model.zip
 popd
 
